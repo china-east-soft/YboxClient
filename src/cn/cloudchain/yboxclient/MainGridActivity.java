dac07375eb410403d108fea0094766797a94a076
Lazzy <lazzy.nan@gmail.com>
2014-02-27 20:40:52 +0800

    modify view

--- src/cn/cloudchain/yboxclient/MainGridActivity.java
+++ src/cn/cloudchain/yboxclient/MainGridActivity.java
@@ -6,23 +6,33 @@ import android.os.Bundle;
 import android.os.Message;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.content.LocalBroadcastManager;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.widget.GridLayout;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.ToggleButton;
 import cn.cloudchain.yboxclient.dialog.CustomDialogFragment;
+import cn.cloudchain.yboxclient.dialog.TaskDialogFragment;
 import cn.cloudchain.yboxclient.face.IDialogService;
 import cn.cloudchain.yboxclient.helper.ApStatusHandler;
 import cn.cloudchain.yboxclient.helper.SetHelper;
+import cn.cloudchain.yboxclient.helper.Util;
 import cn.cloudchain.yboxclient.helper.WeakHandler;
 import cn.cloudchain.yboxclient.server.ApStatusReceiver;
+import cn.cloudchain.yboxclient.task.MobileDataControlTask;
+import cn.cloudchain.yboxclient.views.GridItem1;
 
-public class MainGridActivity extends ActionBarActivity implements
-		OnClickListener, OnCheckedChangeListener {
+public class MainGridActivity extends BaseActionBarActivity implements
+		OnClickListener {
+	final static String TAG = MainGridActivity.class.getSimpleName();
+	private ToggleButton ethernetToggle;
+	private ToggleButton mobileDataToggle;
+	private ToggleButton wifiToggle;
+	private ToggleButton batteryToggle;
+
+	private GridItem1 statusGItem;
+	private GridItem1 settingGItem;
+	private GridItem1 shutdownGItem;
+	private GridItem1 dataGItem;
+
 	private MyHandler handler = new MyHandler(this);
 	private ReceiverHandler receiverHandler = new ReceiverHandler(this);
 	private ApStatusReceiver statusReceiver;
@@ -30,72 +40,34 @@ public class MainGridActivity extends ActionBarActivity implements
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		GridLayout gridLay = new GridLayout(this);
-		gridLay.setColumnCount(10);
-		gridLay.setRowCount(10);
+		setContentView(R.layout.layout_main);
+		ethernetToggle = (ToggleButton) this.findViewById(R.id.status_ethernet);
+		mobileDataToggle = (ToggleButton) this.findViewById(R.id.status_data);
+		wifiToggle = (ToggleButton) this.findViewById(R.id.status_wifi);
+		batteryToggle = (ToggleButton) this.findViewById(R.id.status_battery);
 
-		GridLayout.LayoutParams params0 = new GridLayout.LayoutParams();
-		params0.columnSpec = GridLayout.spec(0, 6, GridLayout.FILL);
-		params0.rowSpec = GridLayout.spec(0, 2, GridLayout.FILL);
-		params0.setMargins(0, 0, 0, 0);
-		Button button0 = new Button(this);
-		button0.setLayoutParams(params0);
-		button0.setText("基本状态");
-		gridLay.addView(button0);
+		statusGItem = (GridItem1) this.findViewById(R.id.tab_status);
+		settingGItem = (GridItem1) this.findViewById(R.id.tab_setting);
+		shutdownGItem = (GridItem1) this.findViewById(R.id.tab_shutdown);
+		dataGItem = (GridItem1) this.findViewById(R.id.tab_mobile_data);
 
-		GridLayout.LayoutParams params1 = new GridLayout.LayoutParams();
-		params1.columnSpec = GridLayout.spec(6, 2, GridLayout.FILL);
-		Button button1 = new Button(this);
-		button1.setId(R.id.tab_status);
-		button1.setLayoutParams(params1);
-		button1.setText("状态");
-		button1.setOnClickListener(this);
-		gridLay.addView(button1);
-
-		GridLayout.LayoutParams params2 = new GridLayout.LayoutParams();
-		params2.columnSpec = GridLayout.spec(8, 2);
-		Button button2 = new Button(this);
-		button2.setId(R.id.tab_setting);
-		button2.setLayoutParams(params2);
-		button2.setText("设置");
-		button2.setOnClickListener(this);
-		gridLay.addView(button2);
-
-		GridLayout.LayoutParams params3 = new GridLayout.LayoutParams();
-		params3.columnSpec = GridLayout.spec(6, 2);
-		Button button3 = new Button(this);
-		button3.setId(R.id.tab_shutdown);
-		button3.setLayoutParams(params3);
-		button3.setText("关机");
-		button3.setOnClickListener(this);
-		gridLay.addView(button3);
-
-		GridLayout.LayoutParams params4 = new GridLayout.LayoutParams();
-		params4.columnSpec = GridLayout.spec(8, 2);
-		ToggleButton button4 = new ToggleButton(this);
-		button4.setId(R.id.tab_mobile_data);
-		button4.setLayoutParams(params4);
-		button4.setTextOff("数据关");
-		button4.setTextOn("数据开");
-		button4.setChecked(MyApplication.getInstance().connType == 2);
-		button4.setOnCheckedChangeListener(this);
-		gridLay.addView(button4);
-
-		GridLayout.LayoutParams params5 = new GridLayout.LayoutParams();
-		params5.columnSpec = GridLayout.spec(0, 10, GridLayout.FILL);
-		params5.rowSpec = GridLayout.spec(2, 6, GridLayout.FILL);
-		Button button5 = new Button(this);
-		button5.setLayoutParams(params5);
-		button5.setText("电视");
-		gridLay.addView(button5);
-
-		setContentView(gridLay);
+		statusGItem.setOnClickListener(this);
+		settingGItem.setOnClickListener(this);
+		shutdownGItem.setOnClickListener(this);
+		dataGItem.setOnClickListener(this);
 	}
 
 	@Override
 	protected void onStart() {
 		super.onStart();
 		registerReceiver();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		refreshBattery();
+		refreshConnType();
 	}
 
 	@Override
@@ -120,26 +92,49 @@ public class MainGridActivity extends ActionBarActivity implements
 			startActivity(intent);
 			break;
 		}
+		case R.id.tab_mobile_data:
+			handler.sendEmptyMessage(MyHandler.ACTION_MOBILE_DATA);
+			break;
 		}
 
 	}
 
-	@Override
-	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-		if (buttonView.getId() == R.id.tab_mobile_data) {
-
-		}
-	}
-
-	private void refreshView() {
+	private void refreshConnType() {
 		int connType = MyApplication.getInstance().connType;
+		dataGItem.setEnabled(connType != 0);
 		switch (connType) {
+		// 无
 		case 0:
+			ethernetToggle.setChecked(false);
+			mobileDataToggle.setChecked(false);
+			dataGItem.setChecked(false);
 			break;
+		// 以太网
 		case 1:
+			ethernetToggle.setChecked(true);
+			mobileDataToggle.setChecked(false);
+			dataGItem.setChecked(false);
 			break;
+		// 手机数据开
 		case 2:
+			ethernetToggle.setChecked(false);
+			mobileDataToggle.setChecked(true);
+			dataGItem.setChecked(true);
 			break;
+		// 手机数据关
+		case 3:
+			ethernetToggle.setChecked(false);
+			mobileDataToggle.setChecked(false);
+			dataGItem.setChecked(false);
+			break;
+		}
+	}
+
+	private void refreshBattery() {
+		boolean low = MyApplication.getInstance().batteryLow;
+		batteryToggle.setChecked(!low);
+		if (low) {
+			Util.toaster(R.string.warn_battery_low);
 		}
 	}
 
@@ -157,7 +152,10 @@ public class MainGridActivity extends ActionBarActivity implements
 				return;
 			switch (msg.what) {
 			case WIFI_MODE_CHANGE:
-				getOwner().refreshView();
+				getOwner().refreshConnType();
+				break;
+			case BATTERY_LOW_CHANGE:
+				getOwner().refreshBattery();
 				break;
 			}
 		}
@@ -169,6 +167,7 @@ public class MainGridActivity extends ActionBarActivity implements
 		}
 		IntentFilter filter = new IntentFilter();
 		filter.addAction(ApStatusReceiver.ACTION_WIFI_MODE_CHANGE);
+		filter.addAction(ApStatusReceiver.ACTION_BATTERY_LOW_CHANGE);
 		LocalBroadcastManager.getInstance(this).registerReceiver(
 				statusReceiver, filter);
 	}
@@ -210,7 +209,12 @@ public class MainGridActivity extends ActionBarActivity implements
 	}
 
 	private void handleMobileData() {
-
+		boolean enable = !dataGItem.isChecked();
+		MobileDataControlTask task = new MobileDataControlTask(this, enable);
+		TaskDialogFragment fragment = TaskDialogFragment.newLoadingFragment(
+				enable ? "正在打开数据" : "正在关闭数据", true);
+		fragment.setTask(task);
+		fragment.show(getSupportFragmentManager(), null);
 	}
 
 	private void handleShutDownAction() {
