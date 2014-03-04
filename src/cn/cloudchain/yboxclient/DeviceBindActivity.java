63ad48b064cbb9fcb5185e44906ddc764c3eac88
Lazzy <lazzy.nan@gmail.com>
2014-03-04 14:23:03 +0800

    添加设备绑定界面：UI

---
+++ src/cn/cloudchain/yboxclient/DeviceBindActivity.java
@@ -0,0 +1,14 @@
+package cn.cloudchain.yboxclient;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class DeviceBindActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.layout_device_bind);
+	}
+
+}
