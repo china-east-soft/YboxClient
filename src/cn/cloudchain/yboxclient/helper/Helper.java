6fe5428da0a6e7c771ffd457353026d7837fc169
Lazzy <lazzy.nan@gmail.com>
2014-03-13 14:01:09 +0800

    添加inputstream转字符串、mac地址获取方法

--- src/cn/cloudchain/yboxclient/helper/Helper.java
+++ src/cn/cloudchain/yboxclient/helper/Helper.java
@@ -1,6 +1,9 @@
 package cn.cloudchain.yboxclient.helper;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
 
 import android.content.Context;
@@ -9,6 +12,12 @@ import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import cn.cloudchain.yboxclient.MyApplication;
 
+/**
+ * 封装一些公用的方法
+ * 
+ * @author lazzy
+ * 
+ */
 public class Helper {
 	private static Helper instance;
 	private Context appContext;
@@ -23,26 +32,13 @@ public class Helper {
 		appContext = MyApplication.getAppContext();
 	}
 
-	public String getGateway(Context mContext) {
-		WifiManager wifi = (WifiManager) mContext
-				.getSystemService(Context.WIFI_SERVICE);
-		DhcpInfo dInfo = wifi.getDhcpInfo();
-		String gateway = long2ip(dInfo.gateway);
-		return gateway;
-	}
-
-	private String long2ip(int ip) {
-		int[] b = new int[4];
-		b[0] = (int) ((ip >> 24) & 0xff);
-		b[1] = (int) ((ip >> 16) & 0xff);
-		b[2] = (int) ((ip >> 8) & 0xff);
-		b[3] = (int) (ip & 0xff);
-		String x;
-		x = Integer.toString(b[3]) + "." + Integer.toString(b[2]) + "."
-				+ Integer.toString(b[1]) + "." + Integer.toString(b[0]);
-		return x;
-	}
-
+	/**
+	 * 获取设备网关地址
+	 * 
+	 * @param context
+	 * @return
+	 * @throws IOException
+	 */
 	public InetAddress getBroadcastAddress(Context context) throws IOException {
 		WifiManager wifi = (WifiManager) context
 				.getSystemService(Context.WIFI_SERVICE);
@@ -55,11 +51,42 @@ public class Helper {
 		return InetAddress.getByAddress(quads);
 	}
 
+	/**
+	 * 获取设备MAC地址
+	 * 
+	 * @param context
+	 * @return
+	 */
 	public String getDevicesMac(Context context) {
 		WifiManager wifi = (WifiManager) context
 				.getSystemService(Context.WIFI_SERVICE);
 		WifiInfo info = wifi.getConnectionInfo();
 		return info.getMacAddress();
+	}
+
+	/**
+	 * 将字节流转换成字符串
+	 * 
+	 * @param inStream
+	 * @return 不可能返回null
+	 */
+	public String transStreamToString(InputStream inStream) {
+		StringBuilder builder = new StringBuilder();
+		if (inStream != null) {
+
+			try {
+				BufferedReader reader = new BufferedReader(
+						new InputStreamReader(inStream));
+				String line = "";
+				while ((line = reader.readLine()) != null) {
+					builder.append(line);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return builder.toString();
 	}
 
 	/**
