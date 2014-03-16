664ecbd0704893a6d06cbf46189e78d7b6278bfc
Lazzy <lazzy.nan@gmail.com>
2014-03-16 20:59:46 +0800

    添加设备MAC地址相关处理方法

--- src/cn/cloudchain/yboxclient/utils/PreferenceUtil.java
+++ src/cn/cloudchain/yboxclient/utils/PreferenceUtil.java
@@ -1,8 +1,11 @@
 package cn.cloudchain.yboxclient.utils;
 
+import java.util.Locale;
+
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.preference.PreferenceManager;
+import android.text.TextUtils;
 import cn.cloudchain.yboxclient.MyApplication;
 
 public class PreferenceUtil {
@@ -17,6 +20,8 @@ public class PreferenceUtil {
 	public static final String PREF_LISENCE_CHECK_STATE = "lisence_check_state";
 	public static final String PREF_PHONE_VERIFY_STARTTIME = "phone_verify_starttime";
 	public static final String PREF_TEMP_PHONE = "temp_phone";
+
+	public static final String BIND_DEVICES = "bind_devices";
 
 	private static SharedPreferences prefSetting = PreferenceManager
 			.getDefaultSharedPreferences(MyApplication.getAppContext());
@@ -70,5 +75,58 @@ public class PreferenceUtil {
 
 	public static boolean getBoolean(String key, boolean defaultValue) {
 		return prefSetting.getBoolean(key, defaultValue);
+	}
+
+	/**
+	 * 每个mac地址均以空格隔开，以空格结尾，大小写区分
+	 * 
+	 * @param mac
+	 */
+	public static void appendDeviceMac(String mac) {
+		String oldStr = prefSetting.getString(BIND_DEVICES, "");
+		StringBuilder builder = new StringBuilder(oldStr);
+		if (!TextUtils.isEmpty(mac) && !oldStr.contains(mac)) {
+			builder.append(mac);
+			builder.append(" ");
+		}
+		Editor editor = prefSetting.edit();
+		editor.putString(BIND_DEVICES, builder.toString());
+		editor.commit();
+		editor = null;
+	}
+
+	/**
+	 * 是否存在该MAC地址，大小写区分
+	 * 
+	 * @param mac
+	 * @return
+	 */
+	public static boolean hasDeviceMac(String mac) {
+		String oldStr = prefSetting.getString(BIND_DEVICES, "");
+		return !TextUtils.isEmpty(mac) && oldStr.contains(mac);
+	}
+
+	/**
+	 * 移除某个特定的mac地址，大小写区分
+	 * 
+	 * @param mac
+	 */
+	public static void removeDeviceMac(String mac) {
+		if (TextUtils.isEmpty(mac)) {
+			return;
+		}
+		String oldStr = prefSetting.getString(BIND_DEVICES, "");
+		int index = oldStr.indexOf(mac);
+		if (index < 0) {
+			return;
+		}
+		StringBuilder builder = new StringBuilder();
+		builder.append(oldStr.subSequence(0, index));
+		builder.append(oldStr.subSequence(index + mac.length() + 1,
+				oldStr.length()));
+		Editor editor = prefSetting.edit();
+		editor.putString(BIND_DEVICES, builder.toString());
+		editor.commit();
+		editor = null;
 	}
 }
