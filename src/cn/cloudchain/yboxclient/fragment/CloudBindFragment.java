0d9f897e65e1fdea2fc76036072d41ee0e856b78
Lazzy <lazzy.nan@gmail.com>
2014-03-23 11:09:29 +0800

    修改公众云账号绑定页面

--- src/cn/cloudchain/yboxclient/fragment/CloudBindFragment.java
+++ src/cn/cloudchain/yboxclient/fragment/CloudBindFragment.java
@@ -1,21 +1,21 @@
 package cn.cloudchain.yboxclient.fragment;
 
 import android.os.Bundle;
+import android.os.Message;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import cn.cloudchain.yboxclient.R;
-import cn.cloudchain.yboxclient.dialog.TaskDialogFragment;
 import cn.cloudchain.yboxclient.helper.CloudHelper;
-import cn.cloudchain.yboxclient.task.BaseFragmentTask;
+import cn.cloudchain.yboxclient.helper.WeakHandler;
 import cn.cloudchain.yboxclient.utils.LogUtil;
-import cn.cloudchain.yboxclient.utils.PreferenceUtil;
 import cn.cloudchain.yboxclient.utils.Util;
 
+import com.baidu.frontia.Frontia;
+import com.baidu.frontia.FrontiaAccount;
 import com.baidu.frontia.FrontiaUser;
 import com.baidu.frontia.api.FrontiaAuthorizationListener.AuthorizationListener;
 
@@ -23,8 +23,17 @@ public class CloudBindFragment extends Fragment {
 	final String TAG = CloudBindFragment.class.getSimpleName();
 
 	private Button test;
-	private boolean isLogin = !TextUtils.isEmpty(PreferenceUtil.getString(
-			PreferenceUtil.BAIDU_AUTU_TOKEN, null));
+	private boolean isLogin = false;
+
+	private MyHandler handler = new MyHandler(this);
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		FrontiaAccount account = Frontia.getCurrentAccount();
+		isLogin = account != null
+				&& (account.getType() == FrontiaAccount.Type.USER);
+	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -62,55 +71,70 @@ public class CloudBindFragment extends Fragment {
 
 					@Override
 					public void onSuccess(FrontiaUser arg0) {
-						isLogin = true;
-						refreshView();
+						handler.sendEmptyMessage(MyHandler.LOGIN_SUCCESS);
 					}
 
 					@Override
 					public void onFailure(int arg0, String arg1) {
 						LogUtil.e(TAG,
 								String.format("code = %d msg = %s", arg0, arg1));
-						Util.toaster("授权失败", null);
+						handler.sendEmptyMessage(MyHandler.LOGIN_FAIL);
 					}
 
 					@Override
 					public void onCancel() {
-						Util.toaster("取消授权", null);
 					}
 				});
 	}
 
 	private void handleLogout() {
-		TaskDialogFragment fragment = TaskDialogFragment.newLoadingFragment(
-				"正在登出...", false);
-		fragment.setTask(new LogoutTask());
-		fragment.show(getFragmentManager(), null);
+		boolean result = CloudHelper.getInstance().logout();
+		handler.sendEmptyMessage(result ? MyHandler.LOGOUT_SUCCESS
+				: MyHandler.LOGOUT_FAIL);
 	}
 
-	private class LogoutTask extends BaseFragmentTask {
-		private final static int RESULT_SUCCESS = 0;
-		private final static int RESULT_FAIL = 1;
+	private static class MyHandler extends WeakHandler<CloudBindFragment> {
+		private static final int LOGIN = 0;
+		private static final int LOGIN_SUCCESS = 1;
+		private static final int LOGIN_FAIL = 2;
+		private static final int LOGOUT = 3;
+		private static final int LOGOUT_SUCCESS = 4;
+		private static final int LOGOUT_FAIL = 5;
 
-		@Override
-		protected Integer doInBackground(Void... params) {
-			return CloudHelper.getInstance().logout() ? RESULT_SUCCESS
-					: RESULT_FAIL;
+		public MyHandler(CloudBindFragment owner) {
+			super(owner);
 		}
 
 		@Override
-		protected void onPostExecute(Integer result) {
-			super.onPostExecute(result);
-			switch (result) {
-			case RESULT_SUCCESS:
-				Util.toaster("登出成功", null);
-				isLogin = false;
-				refreshView();
+		public void handleMessage(Message msg) {
+			super.handleMessage(msg);
+			if (getOwner() == null)
+				return;
+
+			switch (msg.what) {
+			case LOGIN:
+				getOwner().handleLogin();
 				break;
-			case RESULT_FAIL:
+			case LOGIN_SUCCESS:
+				getOwner().isLogin = true;
+				getOwner().refreshView();
+				break;
+			case LOGIN_FAIL:
+				Util.toaster("授权失败", null);
+				break;
+			case LOGOUT:
+				getOwner().handleLogout();
+				break;
+			case LOGOUT_SUCCESS:
+				getOwner().isLogin = false;
+				getOwner().refreshView();
+				break;
+			case LOGOUT_FAIL:
 				Util.toaster("登出失败", null);
 				break;
 			}
 		}
+
 	}
 
 }
